4a1ed5eec58b7119537141d3d73c0eb454e70955
Malicious-Algorithm <fer.gonzalez.gabriel@gmail.com>
2021-10-31 16:38:37 -0300

    2FA with QR-code

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const Express = require('express');
 const app = Express();
 const conectarDB = require('./DB/db');
+const cors = require('cors');
 
 const PORT = 4000 || process.env.PUERTO;
+
+var corsOpt = {
+    origin: '*', //poner nuestro dominio
+    optionsSuccessStatus: 200
+}
 
 conectarDB();
 
 app.use(Express.json());
-
+app.use(cors(corsOpt));
 
 const twoFARoutes = require('./Routes/autenticar');
 
